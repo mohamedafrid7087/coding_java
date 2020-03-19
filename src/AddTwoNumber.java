028a2003dc305cf9109009c3461c37dea3341f7f
Mohamed Afrid <mohamedafrid1996@gmail.com>
2020-03-19 10:29:21 +0500

    no changes

--- src/AddTwoNumber.java
+++ src/AddTwoNumber.java
@@ -1,12 +1,12 @@
 public class AddTwoNumber {
-    int sum(int a, int b){
-    int c;
-    c = a + b;
-    return c;
+    int sum(int a, int b) {
+        int c;
+        c = a + b;
+        return c;
     }
 
-    public static void main(String[] args){
-        AddTwoNumber addTwoNumber= new AddTwoNumber();
+    public static void main(String[] args) {
+        AddTwoNumber addTwoNumber = new AddTwoNumber();
         System.out.println(addTwoNumber.sum(2000, 7000));
     }
 
