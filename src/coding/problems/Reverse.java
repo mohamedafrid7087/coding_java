9f99543e8361a4dd754f01aea8ac18be60a0b732
Mohamed Afrid <mohamedafrid1996@gmail.com>
2020-09-16 15:01:17 +0500

    git commit-m " first sorting problem on reverse the array"

---
+++ src/coding/problems/Reverse.java
@@ -0,0 +1,12 @@
+package coding.problems;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int []myIntegers = {1, 2, 3, 4, 5};
+        for(int i=myIntegers.length-1; i>=0; i--){
+            System.out.print(myIntegers[i] + " ");
+        }
+    }
+
+
+}
