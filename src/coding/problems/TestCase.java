d0500d49107a4e8a0c3b781c7174b114c9048f2f
Mohamed Afrid <mohamedafrid1996@gmail.com>
2020-10-06 13:51:48 +0500

    git commit-m "populate array program done using while loop"

---
+++ src/coding/problems/TestCase.java
@@ -0,0 +1,37 @@
+package coding.problems;
+
+public class TestCase {
+
+    public int[] createsAndPopulatesArray(int[] array) {
+        int evenInd = 1;
+        int oddInd = 0;
+        int n = array.length;
+
+        while (true) {
+            while (evenInd < n && array[evenInd] % 2 == 0)
+                evenInd += 2;
+            while (oddInd < n && array[oddInd] % 2 == 1)
+                oddInd += 2;
+            if (evenInd < n && oddInd < n) {
+                int temp = array[evenInd];
+                array[evenInd] = array[oddInd];
+                array[oddInd] = temp;
+            }
+            else
+                break;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int [] input = {0, 5, 7, 8, 9, 10};
+        TestCase populatesArray = new TestCase();
+        int [] result = populatesArray.createsAndPopulatesArray(input);
+        System.out.print("output : ");
+        for(int item : result){
+            System.out.print(item + ", ");
+        }
+
+    }
+}
+
