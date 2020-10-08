0adc03fde9b57359d50aeabaddcb5f3c1769c6f2
Mohamed Afrid <mohamedafrid1996@gmail.com>
2020-10-08 22:21:35 +0500

    git commit-m "populate array program done using while loop with scanner(system.in)"

---
+++ src/coding/problems/FindArrayElement.java
@@ -0,0 +1,37 @@
+package coding.problems;
+
+import java.util.Scanner;
+
+/**
+ * write the program to find if a element exist in an array.
+ * array = {1, 2, 3, 4, 5}
+ * input = 5 output is true
+ * input = 7 output is false.
+ */
+public class FindArrayElement {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public boolean findElement(int[] array, int element) {
+        for (int i = 0; i < array.length; i++) {
+            if (element == array[i]) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        FindArrayElement problemsArray = new FindArrayElement();
+        Scanner scanner = new Scanner(System.in);
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.print("press to find element in array");
+        int input ;
+        input = scanner.nextInt();
+        System.out.println(" output is " + problemsArray.findElement(array, input));
+    }
+}
